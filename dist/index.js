662e4aa38178df47d89b99b0934dcd667ae11a81
Seb <sebhendowski@gmail.com>
2019-07-15 14:13:46 -0700

    update server import

--- dist/index.js
+++ dist/index.js
@@ -7,13 +7,13 @@ exports.connect = exports.start = void 0;
 
 var _Client = _interopRequireDefault(require("./client/Client"));
 
-var _Server = _interopRequireDefault(require("./server/Server"));
+var _Server = require("./server/Server");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 // export {Server} from './server/Server'
 var start = function start(options) {
-  return _Server["default"].start(options);
+  return _Server.Server.start(options);
 };
 
 exports.start = start;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // export {Server} from './server/Server'
 
 import Client from './client/Client'
-import Server from './server/Server'
+import {Server} from './server/Server'
 
 export const start = (options) => Server.start(options)
 export const connect = options => Client.open(options)
